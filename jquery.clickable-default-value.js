a32763d37898f48008070fa4f694da9369f1913d
Boris Nadion <boris@astrails.com>
2009-05-21 15:18:43 +0300

    initial

---
+++ jquery.clickable-default-value.js
@@ -0,0 +1,52 @@
+/*jquery.clickable-default-value.js */
+/*
+ * jQuery clickable default form values
+ * version: 0.0.1
+ * @requires: jQuery v1.2 or later
+ * Licensed under the GPL license:
+ *   http://www.gnu.org/licenses/gpl.html
+ *
+ * Copyright 2009 Astrails Ltd
+ *	http://astrails.com
+
+ * Usage:
+ * jQuery("form.some-form").clickable_default_value([["#name", "what is your name"], ["#email", "what is your email"]]);
+ */
+
+(function($){
+ $.fn.clickable_default_value = function(opts) {
+
+			function set_text(el, text) {
+				if (!el.val() || "" == el.val()) {
+					el.val(text);
+				}
+			}
+
+    return this.each(function() {
+			var form = $(this);
+
+			$.each(opts, function(i, e) {
+				var elem = $(e[0]);
+				var text = e[1];
+				set_text($(elem), text);
+				$(elem).focus(function() {
+					if ($(this).val() == text) {
+						$(this).val("");
+					}
+				}).blur(function(){
+					set_text($(this), text);
+				});
+			});
+
+			form.submit(function() {
+				$.each(opts, function(i, e) {
+					var el = $(e[0]);
+					if ($(e[0]).val() == e[1]) {
+						el.val("");
+					}
+				});
+				return true;
+			});
+    });
+ };
+})(jQuery);
